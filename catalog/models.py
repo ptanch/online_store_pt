9c84dde3e2f2d617876c24dfc2c23432718e3be5
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-09-30 17:08:13 +0300

    в приложении каталога создайте модели product и category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """Класс для представления продукта"""
+
+    name = models.CharField(
+        max_length=100, verbose_name='Наименование', help_text='Введите наименование продукта'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание продукта',
+        blank=True,
+        null=True
+    )
+    photo = models.ImageField(
+        upload_to='catalog/photo',
+        blank=True,
+        null=True,
+        verbose_name='Фото',
+        help_text='Загрузите фото продукта'
+    )
+    category = models.CharField(
+        max_length=100,
+        verbose_name='Категория',
+        help_text='Введите категорию продукта',
+        blank=True,
+        null=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Описание параметров модели"""
+
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'category']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """Класс для представления категории"""
+
+    name = models.CharField(
+        max_length=100, verbose_name='Категория', help_text='Введите категорию'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание категории',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        """Описание параметров модели"""
+
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
