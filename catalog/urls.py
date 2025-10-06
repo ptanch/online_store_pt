6cd1a81c973533618898680e2a77acd5e3c5d13a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-06 23:51:42 +0300

    контроллер зарегистрирован на url product

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('product/', product, name='product')
 ]
 
 if settings.DEBUG:
