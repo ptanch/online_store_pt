75f801047d1124a37b761d0f421afc4501719c47
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-07 16:21:28 +0300

    прописаны пути к до страницы конкретного продукта, отредактированы контроллеры для подтягивания информации из базы данных

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
@@ -12,5 +15,13 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def product_iphone(request):
-    return render(request, 'product_iphone.html')
+def index(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'base.html', context)
+
+
+def product_iphone(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_iphone.html', context)
