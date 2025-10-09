e2e46c51aba55a82bd5e7d364747cf57c196564c
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-09 12:07:01 +0300

    убран импорт функции index, убран нерелевантный путь

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, index, product_iphone
+from catalog.views import home, contacts, product_iphone
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', index, name='index'),
     path('catalog/<int:pk>/', product_iphone, name='product_iphone')
 ]
 
