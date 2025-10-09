399811599b88352830c30f4353b2bf56a00b4779
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-09 13:11:45 +0300

    изменено название шаблона для страницы с одним товаром

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_iphone
+from catalog.views import home, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('catalog/<int:pk>/', product_iphone, name='product_iphone')
+    path('catalog/<int:pk>/', product_detail, name='product_detail')
 ]
 
 if settings.DEBUG:
--- catalog/views.py
+++ catalog/views.py
@@ -15,7 +15,7 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def product_iphone(request, pk):
+def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
-    return render(request, 'product_iphone.html', context)
+    return render(request, 'product_detail.html', context)
