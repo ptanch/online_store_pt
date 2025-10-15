6f56c98517d62cf03267546e950b29057da24784
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 15:52:42 +0300

    настроена маршрутизация для страницы contacts, для корректного отображения информации на странице

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, contacts, ProductDetailView
+from catalog.views import ProductListView, ContactsView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', ProductListView.as_view(), name='product_list'),
-    path('contacts/', contacts, name='contacts'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
     path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
 
