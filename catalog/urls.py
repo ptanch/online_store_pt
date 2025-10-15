eaeda11c610184c5125f0e725b084b0f8e7afda0
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 14:39:39 +0300

    изменение маршрута на класс

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_detail
+from catalog.views import ProductListView, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('home/', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('catalog/<int:pk>/', product_detail, name='product_detail')
 ]
