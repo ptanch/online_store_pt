5af2482e2763ee40de6d7297b9fe5eadf0410542
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 23:09:40 +0300

    модифицирован вывод и обработка запроса на уровне контроллера для увеличения счетчика просмотров

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from blog.models import Blog
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 
 
 class BlogListView(ListView):
@@ -11,3 +11,16 @@ class BlogCreateView(CreateView):
     model = Blog
     fields = ("title", "content", "preview", "creation_date", "is_published", "views_number")
     success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'blog'
+
+    def get_object(self, queryset=None):
+        """Переопределение метода, чтобы увеличить счётчик просмотров"""
+        obj = super().get_object(queryset)
+        obj.views_number += 1
+        obj.save(update_fields=['views_number'])
+        return obj
