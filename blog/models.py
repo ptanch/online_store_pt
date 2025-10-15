20d1592339da2824089a049b358dd7d0439823d7
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 21:39:21 +0300

    создана новая модель блоговой записи blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержимое', blank=True, null=True)
+    preview = models.ImageField(upload_to='blog/photo', blank=True, null=True, verbose_name='Предварительный просмотр',)
+    creation_date = models.DateTimeField(blank=True, null=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=False)
+    views_number = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+        ordering = ['name', 'creation_date']
+
+    def __str__(self):
+        return self.name
