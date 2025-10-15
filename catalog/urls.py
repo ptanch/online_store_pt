7d96cc7a33e1d845e2ea4c1486b5821cf1e677dd
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 15:03:54 +0300

    изменение маршрутизации для страниц home и product_detail, для корректного отображения информации на странице

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, contacts, product_detail
+from catalog.views import ProductListView, contacts, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', ProductListView.as_view(), name='home'),
+    path('home/', ProductListView.as_view(), name='product_list'),
     path('contacts/', contacts, name='contacts'),
-    path('catalog/<int:pk>/', product_detail, name='product_detail')
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
 
 if settings.DEBUG:
