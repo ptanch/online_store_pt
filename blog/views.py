cb28132f810d44cc04235b9ffde00f572d9de0c7
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 22:43:13 +0300

    созданы классы представления create.view и list.view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from blog.models import Blog
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ("title", "content", "preview", "creation_date", "is_published", "views_number")
+    success_url = reverse_lazy('blog:blog_list')
