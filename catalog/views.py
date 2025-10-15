34c871680f382244b8137060175980a97a8a8d65
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-15 15:50:22 +0300

    контроллер для страницы contacts.html был переведен с FBV на CBV, реализован метод post

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
@@ -10,10 +10,18 @@ class ProductListView(ListView):
     #  catalog/product_list.html
 
 
-def contacts(request):
-    if request.method == 'POST':
-        return render(request, 'contacts.html',{"success_message": "Данные успешно отправлены"})
-    return render(request, 'contacts.html')
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context["success_message"] = "Данные успешно отправлены"
+        return self.render_to_response(context)
+
+# def contacts(request):
+#     if request.method == 'POST':
+#         return render(request, 'contacts.html',{"success_message": "Данные успешно отправлены"})
+#     return render(request, 'contacts.html')
 
 
 class ProductDetailView(DetailView):
