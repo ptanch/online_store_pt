730f0331217c1f1575d25ef1ca44af14b7005bac
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-16 22:08:49 +0300

    скорректированы маршруты до всех страниц: убран blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from blog.apps import BlogConfig
+from django.conf import settings
+from django.conf.urls.static import static
 
 from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
@@ -7,9 +9,12 @@ from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog/', BlogListView.as_view(), name='blog_list'),
-    path('blog/create', BlogCreateView.as_view(), name='blog_create'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-    path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
-    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete')
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
