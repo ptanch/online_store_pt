2b24353b6d6c11ce0c9f4178e8395b15db1354a8
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-16 18:14:45 +0300

    зарегистрирована модель blog для отображения в админке

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "preview", "creation_date", "is_published", "views_number")
+    list_filter = ('is_published', 'creation_date')
+    search_fields = ('title', 'content')
