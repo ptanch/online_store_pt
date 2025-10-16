9873e17a870106c940cdd22ff8904dcb7eaf65fe
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-16 16:44:10 +0300

    добавлена маршрутизация для новых классов из контроллера

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from blog.apps import BlogConfig
 
-from blog.views import BlogListView, BlogCreateView
+from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 
 app_name = BlogConfig.name
@@ -9,4 +9,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/create', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/', BlogDetailView(), name='blog_detail'),
+    path('blog/<int:pk>/update/', BlogUpdateView(), name='blog_update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView(), name='blog_delete')
 ]
