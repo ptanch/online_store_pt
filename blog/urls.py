9527e3a9781bf5f7cf707143ebff00bfc4b86d63
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-16 18:11:40 +0300

    исправлены пути для blog.detail.view, blog.update.view, blog.delete.view

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/create', BlogCreateView.as_view(), name='blog_create'),
-    path('blog/<int:pk>/', BlogDetailView(), name='blog_detail'),
-    path('blog/<int:pk>/update/', BlogUpdateView(), name='blog_update'),
-    path('blog/<int:pk>/delete/', BlogDeleteView(), name='blog_delete')
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete')
 ]
