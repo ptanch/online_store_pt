236384ed7f187e6c21dae7a5a469ddd43fb31b6e
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-22 12:46:50 +0300

    добавлен статический метод проверки отрицательной цены, добавлен метод валидации всех форм

--- catalog/forms.py
+++ catalog/forms.py
@@ -21,7 +21,7 @@ class ProductForm(ModelForm):
         for word in self.FORBIDDEN_WORDS:
             if word.lower() in field_value.lower():
                 raise ValidationError(
-                    f"{field_label} не может содержать запрещённое слово: 'word'"
+                    f"{field_label} не может содержать запрещённое слово: '{word}'"
                 )
 
     def clean_name(self):
@@ -33,3 +33,24 @@ class ProductForm(ModelForm):
         description = self.cleaned_data.get('description', '')
         self._validate_forbidden_words(description, "Описание")
         return description
+
+    @ staticmethod
+    def _validate_price(price):
+        if price is None:
+            raise ValidationError('Цена должна быть указана')
+        if price <= 0:
+            raise ValidationError('Цена не может быть меньше нуля')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+        price = cleaned_data.get('price')
+
+        if price == 0 and ('бесплатно' in name.lower() or 'бесплатно' in description.lower()):
+            raise ValidationError('Если цена 0, не указывайте слово "бесплатно" — это и так очевидно.')
+
+        if price is not None:
+            self._validate_price(price)
+
+        return cleaned_data
