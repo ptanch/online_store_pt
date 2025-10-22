5ac5088341d7c962c43e02857903c208d2d3c5f6
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-22 20:01:29 +0300

    зарегистрированы необходимые контроллеры для работы с продуктом

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ContactsView, ProductDetailView
+from catalog.views import (
+    ProductListView,
+    ProductCreateView,
+    ContactsView,
+    ProductDetailView,
+    ProductUpdateView,
+    ProductDeleteView
+)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', ProductListView.as_view(), name='product_list'),
+    path('catalog/', ProductListView.as_view(), name='product_list'),
+    path('catalog/create/', ProductCreateView.as_view(), name='product_create'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
 
 if settings.DEBUG:
