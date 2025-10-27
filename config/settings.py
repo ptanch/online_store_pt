b72d56783d3a61730af08be24cf209d5ca621aaa
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-27 18:02:28 +0300

    создано и зарегистрировано новое приложение users

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 load_dotenv(override=True)
 
-from django.conf.global_settings import STATICFILES_DIRS
+from django.conf.global_settings import STATICFILES_DIRS, AUTH_USER_MODEL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
