1387c0a86f3e5ffbbd47c59373a9af0310167f11
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-27 22:27:55 +0300

    создан urls.py для отображения страницы с логином

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name="login.html"))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
