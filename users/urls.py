dca3469e7bdfc313640d77a95468fe50c98cbe5d
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-27 23:11:39 +0300

    добавлен путь для выхода пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="login.html"))
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
 ]
 
 if settings.DEBUG:
