2103a09cf9eb9a7085232c00bce0f9440509bd0b
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-10-28 13:04:59 +0300

    для контроллеров для создания, изменения, удаления и просматривания конкретного продукта добавлен LoginRequiredMixin, чтобы процесс мог выполняться только авторизованными пользователями

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -13,37 +14,44 @@ from catalog.models import Product
 
 
 class ProductListView(ListView):
+    """Общедоступный список товаров"""
     model = Product
     #  catalog/product_list.html
 
 
 class ContactsView(TemplateView):
+    """Страница с отображением контактов"""
     template_name = 'catalog/contacts.html'
 
     def post(self, request, *args, **kwargs):
+        """Сообщение при отправке данных с помощью формы контактов"""
         context = self.get_context_data(**kwargs)
         context["success_message"] = "Данные успешно отправлены"
         return self.render_to_response(context)
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    """Просмотр конкретного товара - только для авторизованных"""
     model = Product
     #  catalog/product_detail.html
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    """Создание товара - только для авторизованных"""
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_create')
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    """Обновление информации о товаре - только для авторизованных"""
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    """Удаление товара - только для авторизованных"""
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
