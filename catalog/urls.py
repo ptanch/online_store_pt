2fe2dd2f819dd39e4922d8a5f78c079cf96c10ba
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-05 22:56:18 +0300

    настроено кеширование для страницы, которая отображает информацию об одном продукте

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (
     ProductListView,
@@ -16,7 +18,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('catalog/', ProductListView.as_view(), name='product_list'),
     path('catalog/create/', ProductCreateView.as_view(), name='product_create'),
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 
